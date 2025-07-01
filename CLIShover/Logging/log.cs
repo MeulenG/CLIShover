08d490ceaf60de861c95dd87ad5f5d08c43b79c0
MeulenG <olivermeulengracht@protonmail.com>
2025-07-01 23:40:13 +0200

    Logging: VT 100 logging

---
+++ CLIShover/Logging/log.cs
@@ -0,0 +1,22 @@
+using System.Reflection.Emit;
+
+namespace CLIShover.Logging
+{
+    public class Logging
+    {
+        // DONE or FAILED using VT100, Format will be [ DONE ] or [ FAILED ]
+        public static void Log(string message, bool success)
+        {
+            var status = success ? " DONE " : " FAILED ";
+            switch (success)
+            {
+                case true:
+                    Console.WriteLine($"[\x1b[32m{status}\x1b[0m] {message}"); // Green
+                    break;
+                case false:
+                    Console.WriteLine($"[\x1b[31m{status}\x1b[0m] {message}"); // Red
+                    break;
+            }
+        }
+    }
+}
